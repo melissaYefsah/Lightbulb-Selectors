43162e6ac5f6fb0760e2630cf63813575ea0cb2b
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-10-22 14:33:36 -0400

    Add the javaScript code

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Write your code here
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+const h3 = document.querySelector(".subtitle");
+let count = 0; // Change const to let
+
+const updateCounter = () => {
+    h3.innerHTML = `You've clicked the lights ${count} times`;
+};
+
+const toggleBulb = (bulb) => {
+    bulb.classList.toggle("active");
+};
+
+bulb1.addEventListener("click", function() {
+    count++;
+    updateCounter();
+    toggleBulb(this);
+});
+
+bulb2.addEventListener("click", function() {
+    count++;
+    updateCounter();
+    toggleBulb(this);
+});
+
+bulb3.addEventListener("click", function() {
+    count++;
+    updateCounter();
+    toggleBulb(this);
+});
