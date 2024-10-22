dde4ed812f37b92e8c2ca2c289ce94c7967aaeb4
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-10-22 14:35:49 -0400

    Added javaScript code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const bulb1 = document.querySelector("#lightbulb1");
 const bulb2 = document.querySelector("#lightbulb2");
 const bulb3 = document.querySelector("#lightbulb3");
 const h3 = document.querySelector(".subtitle");
-let count = 0; // Change const to let
+let count = 0; 
 
 const updateCounter = () => {
     h3.innerHTML = `You've clicked the lights ${count} times`;
